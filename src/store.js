cee3150059975130769684bb6c27de49db44e421
martinaapalazzi <martinaapalazzi@gmail.com>
2024-01-24 11:53:45 +0100

    edit

--- src/store.js
+++ src/store.js
@@ -4,6 +4,6 @@ export const store = reactive ({
     baseUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
     archetypesUrl: 'https://db.ygoprodeck.com/api/v7/archetypes.php',
     cards: [],
+    archetypes: [],
     searchArchetype: 'Select archetype',
-    archetypes: []
 });
